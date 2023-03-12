fea924e6365b73de14e53d41421b5568ff242eb4
Dipendra Singh <91822875+Dipendra6377@users.noreply.github.com>
2023-03-12 22:08:23 +0530

    Added solution - LeetHub

--- Geek's Training - GFG/geeks-training.java
+++ Geek's Training - GFG/geeks-training.java
@@ -27,36 +27,29 @@ class GFG{
 // } Driver Code Ends
 
 
+//User function Template for Java
+
 class Solution{
     public int maximumPoints(int points[][],int n){
-        int[][] dp= new int[n][4];
-        for (int[] row: dp)
-            Arrays.fill(row, -1);
-            
-        return func(n-1,3,points,dp);
-    }
-    public int func(int day,int last,int[][] point,int[][] dp) {
+        //code here
+        int[][] dp =new int[n][4];
+        dp[0][0] = Math.max(points[0][1], points[0][2]);
+        dp[0][1] = Math.max(points[0][0], points[0][2]);
+        dp[0][2] = Math.max(points[0][0], points[0][1]);
+        dp[0][3] = Math.max(points[0][0], Math.max(points[0][1], points[0][2]));
         
-        if(dp[day][last]!=-1) return dp[day][last];
-        
-        if(day==0){
-            int maxi=0;
-            for(int i=0;i<3;i++){
-                if(i!=last){
-                    maxi=Math.max(point[0][i],maxi);
+        for(int day=1;day<n;day++){
+            for(int last=0;last<4;last++){
+                dp[day][last]=0;
+                
+                for(int task=0;task<=2;task++){
+                    if (task != last) {
+                        int activity = points[day][task] + dp[day - 1][task];
+                        dp[day][last] = Math.max(dp[day][last], activity);
+                    }
                 }
             }
-            return dp[day][last] =maxi;
         }
-        
-        int maxi=0;
-        
-        for(int i=0;i<3;i++){
-            if(i!=last){
-                int points = point[day][i] +func(day-1,i,point,dp);
-                maxi =Math.max(points,maxi);
-            }
-        }
-        return dp[day][last] = maxi;
+        return dp[n-1][3];
     }
 }

446ff5bb5446fc76b93195af877ee89572ed1b30
Dipendra Singh <91822875+Dipendra6377@users.noreply.github.com>
2023-03-18 22:00:02 +0530

    Time: 56 ms (73.17%), Space: 50.4 MB (64.51%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -0,0 +1,32 @@
+class BrowserHistory {
+
+    List<String> list;
+    int total = 0,curr = 0;
+
+    public BrowserHistory(String homepage) {
+        list = new ArrayList<String>();
+        list.add(homepage);
+        total++;
+        curr++;
+    }
+
+    public void visit(String url) {
+        if (list.size() > curr) {
+            list.set(curr, url);
+        } else {
+            list.add(url);
+        }
+        curr++;
+        total = curr;
+    }
+
+    public String back(int steps) {
+        curr = Math.max(1, curr - steps);
+        return list.get(curr - 1);
+    }
+
+    public String forward(int steps) {
+        curr = Math.min(total, curr + steps);
+        return list.get(curr - 1);
+    }
+}

8b3763cbb6e7622a1fe557482384a5ca122cbb64
Dipendra Singh <91822875+Dipendra6377@users.noreply.github.com>
2023-05-15 12:06:23 +0530

    Added solution - LeetHub

---
+++ Rod Cutting - GFG/rod-cutting.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class RodCutting {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+            Solution ob = new Solution();
+            System.out.println(ob.cutRod(arr, n));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    public int cutRod(int price[], int n) {
+        //code here
+        int[][] dp=new int[n+1][n+1];
+        
+        int[] len =new int[n];
+        
+        for(int i=0;i<n;i++){
+            len[i]=i+1;        
+        }
+        
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        
+        for(int i=0;i<=n;i++){
+            dp[0][i]=0;
+        }
+        
+        
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=n;j++){
+                if(len[i-1]<=j){
+                    dp[i][j]=Math.max(price[i-1]+dp[i][j-len[i-1]],dp[i-1][j]);
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][n];
+    }
+}
